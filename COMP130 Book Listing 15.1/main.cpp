d0f5bd696038675ca5fba7b5b3031d768df711a2
aberry5621 <aberry5621@gmail.com>
2016-11-28 09:42:04 -0800

    Initial Commit

---
+++ COMP130 Book Listing 15.1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  COMP130 Book Listing 15.1
+//
+//  Created by ax on 11/28/16.
+//  Copyright © 2016 COMP130. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
